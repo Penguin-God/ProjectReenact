a32d648eab9eb3cb724eb31d8258640456c4946e
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-02-27 23:41:24 +0900

    이벤트 규칙 수정1

--- ProjectReenact/Assets/Script/SimulationBehaviour.cs
+++ ProjectReenact/Assets/Script/SimulationBehaviour.cs
@@ -5,29 +5,20 @@ using UnityEngine;
 public class SimulationBehaviour : MonoBehaviour
 {
     [SerializeField] SimulationConditionContainer simulationCondition;
-    [SerializeField] int _currnetProgress;
     readonly HashSet<int> _executedEvents = new();
 
     public void TryReenact(IEnumerable<int> ids)
     {
-        SimulationProgressData progressData = simulationCondition.GetProgressData(_currnetProgress);
         foreach (SimulationData data in simulationCondition.SimulationConditions)
         {
-            // 시뮬 실행 조건 : 현재 진행도에 맞고 오브젝트가 맞을 때
-            if (progressData.AbleSimulations.Contains(data.SimulationId) && ids.Contains(data.Id1) && ids.Contains(data.Id2))
+            if (CheckIds(ids, data) && SeemPreconditionEvents(data))
             {
                 data.DoSimulation();
-
-                // 이벤트 실행됐으니 진행도 갱신
-                UpdateProgress(progressData, data);
+                _executedEvents.Add(data.SimulationId);
             }
         }
     }
 
-    void UpdateProgress(SimulationProgressData progressData, SimulationData data)
-    {
-        _executedEvents.Add(data.SimulationId);
-        if (progressData.AdvancementSimulations.All(x => _executedEvents.Contains(x)))
-            _currnetProgress++;
-    }
+    bool CheckIds(IEnumerable<int> ids, SimulationData data) => ids.Contains(data.Id1) && ids.Contains(data.Id2);
+    bool SeemPreconditionEvents(SimulationData data) => data.PreconditionEvents.All(x => _executedEvents.Contains(x));
 }
