d68ef8aa01640a9293ae0b1957b4b881da6d266f
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-05-26 16:59:43 +0900

    행동 방식 수정

--- ProjectReenact/Assets/Script/InteractObject.cs
+++ ProjectReenact/Assets/Script/InteractObject.cs
@@ -3,5 +3,6 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class InteractObject : MonoBehaviour
 {
-
+    public string objName;
+    public ActionType[] albeActions;
 }
--- ProjectReenact/Assets/Script/ReenactSystem.cs
+++ ProjectReenact/Assets/Script/ReenactSystem.cs
@@ -1,18 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+
+public enum ActionType
+{
+    Move,
+    Get,
+    PutDown,
+}
 
 public class ReenactSystem : MonoBehaviour
 {
     public LayerMask clickableLayer;
     Actor actor;
-    InteractObject interactObj;
     [SerializeField] int currentActionPhase;
-    [SerializeField] Button moveBtn;
-
-    void Awake()
-    {
-        moveBtn.onClick.AddListener(OnclickActoinButton);
-    }
+    [SerializeField] Transform actoinBthParent;
+    [SerializeField] GameObject actionBtn;
 
     // 액터 먼저 클릭하고 저장하고 오브젝트 클릭하고 저장하고 행동 선택해서 이동하기
     void Update()
@@ -28,18 +31,48 @@ public class ReenactSystem : MonoBehaviour
 
             if(currentActionPhase == 0 && hit.collider.TryGetComponent(out actor))
                 currentActionPhase = 1;
-            else if(currentActionPhase == 1 && hit.collider.TryGetComponent(out interactObj))
+            else if(currentActionPhase == 1 && hit.collider.TryGetComponent(out InteractObject interactObj))
+            {
+                foreach (var action in interactObj.albeActions)
+                {
+                    var btn = Instantiate(actionBtn, actoinBthParent).GetComponent<Button>();
+                    btn.GetComponentInChildren<TextMeshProUGUI>().text = action.ToString();
+                    btn.onClick.AddListener(() => OnclickActoinButton(interactObj, action));
+                }
                 currentActionPhase = 2;
+            }
         }
     }
 
-    void OnclickActoinButton()
+    // 이런식으로 오브젝트 없어지기, 조건에 따라 행동에 대한 이벤트 실행하기. (유물 3개 모으고 삼각형 밖에서 올바른 상형문자 선택 시 마을 없어지기)
+    // 연출은 하얀색 화면으로 바뀐 후 텅 비기
+    // 오브젝트 상호작용으로 변경하기. 이동도 액터, 위치 선택 후 이동으로 고르면 됨
+    void OnclickActoinButton(InteractObject interactObject, ActionType actionType)
     {
         if (currentActionPhase != 2) return;
-        
-        actor.transform.position = interactObj.transform.position;
+
+        Action(actor, interactObject, actionType);
         actor = null;
-        interactObj = null;
         currentActionPhase = 0;
+        foreach (Transform child in actoinBthParent)
+            Destroy(child.gameObject);
+    }
+
+    void Action(Actor actor, InteractObject interactObject, ActionType actionType)
+    {
+        switch (actionType)
+        {
+            case ActionType.Move:
+                actor.transform.position = interactObject.transform.position;
+                actor.transform.position = new Vector3(actor.transform.position.x, actor.transform.position.y, -1);
+                break;
+            case ActionType.Get:
+                interactObject.gameObject.SetActive(false);
+                break;
+            case ActionType.PutDown:
+                break;
+            default:
+                break;
+        }
     }
 }
