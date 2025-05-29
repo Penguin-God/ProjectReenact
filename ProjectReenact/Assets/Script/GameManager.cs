6ddf954c04e44f86aba6775b1e0110f5a917631d
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-05-29 10:14:26 +0900

    조사 기능 추가

--- ProjectReenact/Assets/Script/GameManager.cs
+++ ProjectReenact/Assets/Script/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] Button reenactBtn;
     [SerializeField] string[] conditionIds; // string id로 변경
     [SerializeField] DialogueSystem dialogueSystem;
+    [SerializeField] SpriteRenderer mural1;
+    [SerializeField] Sprite mural1Origin;
+
+    [SerializeField] SpriteRenderer mural2;
+    [SerializeField] Sprite mural2Origin;
 
     void Awake()
     {
@@ -23,5 +28,11 @@ public class GameManager : MonoBehaviour
     {
         if (conditionIds.All(x => dialogueSystem.IsSeen(x)))
             reenactBtn.gameObject.SetActive(true);
+
+        if(dialogueSystem.IsSeen("유물재연1"))
+            mural1.sprite = mural1Origin;
+
+        //if (dialogueSystem.IsSeen("유물2"))
+        //    mural2.sprite = mural2Origin;
     }
 }
