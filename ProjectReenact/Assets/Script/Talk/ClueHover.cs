daa7e5e8916259f705429bb01ebb4582fed42c9c
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2025-07-19 18:35:34 +0900

    툴팁 재구현

--- ProjectReenact/Assets/Script/Talk/ClueHover.cs
+++ ProjectReenact/Assets/Script/Talk/ClueHover.cs
@@ -2,40 +2,40 @@ using UnityEngine;
 
 public class ClueHover : MonoBehaviour
 {
-    //Camera cam;
-    //NodeBehaviour lastNode;  // 마지막에 호버 중이던 단서 추적
+    Camera cam;
+    ClueBehaviour lastClue;  // 마지막에 호버 중이던 단서 추적
 
-    //void Awake()
-    //{
-    //    cam = Camera.main;
-    //}
+    void Awake()
+    {
+        cam = Camera.main;
+    }
 
-    //void Update()
-    //{
-    //    Vector2 worldPoint = cam.ScreenToWorldPoint(Input.mousePosition);
-    //    // 레이어 최적화 하고 싶으면 LayerMask 써서 Clue 레이어만 걸러내세요
-    //    RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+    void Update()
+    {
+        Vector2 worldPoint = cam.ScreenToWorldPoint(Input.mousePosition);
+        // 레이어 최적화 하고 싶으면 LayerMask 써서 Clue 레이어만 걸러내세요
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-    //    if (hit.collider != null)
-    //    {
-    //        var node = hit.collider.GetComponent<NodeBehaviour>();
-    //        if (node != null)
-    //        {
-    //            if (node != lastNode)
-    //            {
-    //                // 새로운 클루로 호버 변경
-    //                TooltipManager.Instance.Show(node.data.name);
-    //                lastNode = node;
-    //            }
-    //            return;
-    //        }
-    //    }
+        if (hit.collider != null)
+        {
+            var clue = hit.collider.GetComponent<ClueBehaviour>();
+            if (clue != null)
+            {
+                if (clue != lastClue)
+                {
+                    // 새로운 클루로 호버 변경
+                    TooltipManager.Instance.Show(clue.type.type);
+                    lastClue = clue;
+                }
+                return;
+            }
+        }
 
-    //    // Clue 위에 없으면 툴팁 숨김
-    //    if (lastNode != null)
-    //    {
-    //        TooltipManager.Instance.Hide();
-    //        lastNode = null;
-    //    }
-    //}
+        // Clue 위에 없으면 툴팁 숨김
+        if (lastClue != null)
+        {
+            TooltipManager.Instance.Hide();
+            lastClue = null;
+        }
+    }
 }
